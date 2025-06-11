ca41af302fe29dc0d58fad14b9f43ea25988f086
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-06-11 22:20:24 +0200

    Init Employee DTO

---
+++ server/src/main/java/com/yasin/erp/model/dto/EmployeeReqDTO.java
@@ -0,0 +1,11 @@
+package com.yasin.erp.model.dto;
+
+import lombok.Data;
+
+@Data
+public class EmployeeReqDTO {
+    private String username;
+    private String password;
+    private String email;
+
+}
