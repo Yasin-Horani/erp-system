129f125cae9eb9359d92ef0a75faed7aa7961f64
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-06-22 16:29:18 +0200

    init EmployeeResponseDto

---
+++ server/src/main/java/com/yasin/erp/model/dto/EmployeeResponseDto.java
@@ -0,0 +1,15 @@
+package com.yasin.erp.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeResponseDto {
+    private String username;
+    private String email;
+}
