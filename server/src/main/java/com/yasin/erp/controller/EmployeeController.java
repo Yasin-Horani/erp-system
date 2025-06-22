3fdaffad3c47a64affeb27e141bf6a727bb5e44f
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-06-22 15:36:49 +0200

    init save controller

--- server/src/main/java/com/yasin/erp/controller/EmployeeController.java
+++ server/src/main/java/com/yasin/erp/controller/EmployeeController.java
@@ -1,12 +1,17 @@
 package com.yasin.erp.controller;
 
 import com.yasin.erp.model.dto.EmployeeReqDTO;
+import com.yasin.erp.model.entity.Employee;
+import com.yasin.erp.repository.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/employees")
 public class EmployeeController {
 
+    @Autowired
+    private EmployeeRepo employeeRepo;
 
     @GetMapping(path = "/employee")
     public String getEmployee(){
@@ -23,11 +28,10 @@ public class EmployeeController {
         return "<h1 style='color: blue;'>"+username+"</h1>";
     }
 
+    // save new employee
     @PostMapping(path = "/save-employee")
-    public void save(@RequestBody EmployeeReqDTO employeeReqDTO){
-        System.out.println(employeeReqDTO);
-        System.out.println(employeeReqDTO.getUsername());
-        System.out.println(employeeReqDTO.getPassword());
-        System.out.println(employeeReqDTO.getEmail());
+    public void save(@RequestBody EmployeeReqDTO req){
+        Employee employee = Employee.toEntity(req);
+        this.employeeRepo.save(employee);
     }
 }
