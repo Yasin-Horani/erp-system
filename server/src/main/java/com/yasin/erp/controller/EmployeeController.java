ce2c59a8ebf54649cc6d0745bbe3b012a6b4eb91
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-06-22 16:30:18 +0200

    to return save data

--- server/src/main/java/com/yasin/erp/controller/EmployeeController.java
+++ server/src/main/java/com/yasin/erp/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.yasin.erp.controller;
 
 import com.yasin.erp.model.dto.EmployeeReqDTO;
+import com.yasin.erp.model.dto.EmployeeResponseDto;
 import com.yasin.erp.model.entity.Employee;
 import com.yasin.erp.model.mapper.EmployeeMapper;
 import com.yasin.erp.repository.EmployeeRepo;
@@ -18,24 +19,25 @@ public class EmployeeController {
     private EmployeeMapper employeeMapper;
 
     @GetMapping(path = "/employee")
-    public String getEmployee(){
+    public String getEmployee() {
         return "<h1 style='color: red;'>Yasin</h1>";
     }
 
     @GetMapping(path = "/employee-by-id")
-    public String getEmployeeById(@RequestParam(name = "id") int id){
-        return "<h1 style='color: green;'>Yasin</h1>"+ id;
+    public String getEmployeeById(@RequestParam(name = "id") int id) {
+        return "<h1 style='color: green;'>Yasin</h1>" + id;
     }
 
     @GetMapping(path = "/employee/{username}/username")
-    public String getEmployeeUsername(@PathVariable(name = "username") String username){
-        return "<h1 style='color: blue;'>"+username+"</h1>";
+    public String getEmployeeUsername(@PathVariable(name = "username") String username) {
+        return "<h1 style='color: blue;'>" + username + "</h1>";
     }
 
     // save new employee
     @PostMapping(path = "/save-employee")
-    public void save(@RequestBody EmployeeReqDTO req){
+    public EmployeeResponseDto save(@RequestBody EmployeeReqDTO req) {
         Employee employee = this.employeeMapper.toEnity(req);
-        this.employeeRepo.save(employee);
+        Employee saveEmployee = this.employeeRepo.save(employee);
+        return this.employeeMapper.toRespDto(saveEmployee);
     }
 }
--- server/src/main/java/com/yasin/erp/model/mapper/EmployeeMapper.java
+++ server/src/main/java/com/yasin/erp/model/mapper/EmployeeMapper.java
@@ -1,10 +1,13 @@
 package com.yasin.erp.model.mapper;
 
 import com.yasin.erp.model.dto.EmployeeReqDTO;
+import com.yasin.erp.model.dto.EmployeeResponseDto;
 import com.yasin.erp.model.entity.Employee;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface EmployeeMapper {
     Employee toEnity(EmployeeReqDTO dto);
+
+    EmployeeResponseDto toRespDto(Employee entity);
 }
