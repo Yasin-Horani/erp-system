0362a15c947b7ab28423227830ac8cd5b87efe24
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-07-01 22:34:20 +0200

    implement the employee interface and rename

--- server/src/main/java/com/yasin/erp/service/impl/EmployeeServiceImp.java
+++ server/src/main/java/com/yasin/erp/service/impl/EmployeeServiceImp.java
@@ -1,31 +1,33 @@
 package com.yasin.erp.service.impl;
 
 import com.yasin.erp.model.dto.EmployeeReqDTO;
-import com.yasin.erp.model.dto.EmployeeResponseDto;
+import com.yasin.erp.model.dto.EmployeeResDto;
 import com.yasin.erp.model.entity.Employee;
 import com.yasin.erp.model.mapper.EmployeeMapper;
 import com.yasin.erp.repository.EmployeeRepo;
+import com.yasin.erp.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 @Service
-public class EmployeeService {
+public class EmployeeServiceImp implements EmployeeService {
     @Autowired
     private EmployeeRepo employeeRepo;
 
     @Autowired
     private EmployeeMapper employeeMapper;
 
-    public EmployeeResponseDto addNewEmpolyee(@RequestBody EmployeeReqDTO req){
+    @Override
+    public EmployeeResDto addNewEmpolyee(EmployeeReqDTO req) {
         Employee employee = this.employeeMapper.toEntity(req);
         Employee saveEmployee = this.employeeRepo.save(employee);
         return this.employeeMapper.toRespDto(saveEmployee);
     }
 
-    public List<Employee> getAllEmplyee(){
+    @Override
+    public List<Employee> getAllEmplyee() {
         return this.employeeRepo.findAll();
     }
 }
