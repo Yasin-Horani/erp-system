ae1ac3f0c3f18970f8e1796714dea7e830653af0
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-07-04 19:04:14 +0200

    init updateEmployee in impl

--- server/src/main/java/com/yasin/erp/service/EmployeeService.java
+++ server/src/main/java/com/yasin/erp/service/EmployeeService.java
@@ -2,14 +2,18 @@ package com.yasin.erp.service;
 
 import com.yasin.erp.model.dto.EmployeeReqDTO;
 import com.yasin.erp.model.dto.EmployeeResDto;
+import com.yasin.erp.model.dto.UpdateEmployeeReqDTO;
 import com.yasin.erp.model.entity.Employee;
 
 import java.util.List;
 
 public interface EmployeeService {
     // add new employee
-    EmployeeResDto addNewEmpolyee(EmployeeReqDTO req);
+    EmployeeResDto saveEmployee(EmployeeReqDTO req);
 
     // get all employee
     List<Employee> getAllEmplyee();
+
+    //update employee
+    EmployeeResDto updateEmployee(UpdateEmployeeReqDTO Id);
 }
