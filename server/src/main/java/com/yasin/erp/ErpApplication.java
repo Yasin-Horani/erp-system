a104b1c044801c498f6c7bce1c9e6dd451131419
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-07-13 22:33:55 +0200

    Modify the code

--- server/src/main/java/com/yasin/erp/ErpApplication.java
+++ server/src/main/java/com/yasin/erp/ErpApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ErpApplication {
-
+	// concept
 	public static void main(String[] args) {
 		SpringApplication.run(ErpApplication.class, args);
 	}
