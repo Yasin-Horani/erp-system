1ff68276168b84651828b2f173fb5fa2b983cc73
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-08-20 16:43:58 +0200

    add ExceptionHandler

--- server/src/main/java/com/yasin/erp/controller/v2/EmployeeControllerV2.java
+++ server/src/main/java/com/yasin/erp/controller/v2/EmployeeControllerV2.java
@@ -7,7 +7,13 @@ import com.yasin.erp.model.dto.v2.EmployeeReqV2DTO;
 import com.yasin.erp.service.v2.EmployeeServiceV2;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
 
 
 @RestController
@@ -21,5 +27,11 @@ public class EmployeeControllerV2 {
     @PostMapping(value = "/employee")
     public EmployeeResDTO save(@RequestBody @Valid EmployeeReqV2DTO req) {
         return this.employeeServiceV2.saveEmployee(req);
+    }
+
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<List> handleBindException(BindException e) {
+        return new ResponseEntity<>(e.getAllErrors(), HttpStatus.BAD_REQUEST);
     }
 }
