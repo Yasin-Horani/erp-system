b6d484ed2e907d4997103a7fc1c65caa3239b97c
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-08-20 17:00:07 +0200

    Clean up controller

--- server/src/main/java/com/yasin/erp/controller/v2/EmployeeControllerV2.java
+++ server/src/main/java/com/yasin/erp/controller/v2/EmployeeControllerV2.java
@@ -7,17 +7,10 @@ import com.yasin.erp.model.dto.v2.EmployeeReqV2DTO;
 import com.yasin.erp.service.v2.EmployeeServiceV2;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.*;
 
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+
 
 
 @RestController
@@ -32,19 +25,4 @@ public class EmployeeControllerV2 {
     public EmployeeResDTO save(@RequestBody @Valid EmployeeReqV2DTO req) {
         return this.employeeServiceV2.saveEmployee(req);
     }
-
-
-    @ExceptionHandler(BindException.class)
-    public ResponseEntity<Map<String, List<String>>> handleBindException(BindException e) {
-        List<String> errors = e.getAllErrors()
-                .stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .collect(Collectors.toList());
-
-        Map<String, List<String>> errMap = new HashMap<>();
-        errMap.put("errors", errors);
-
-        return new ResponseEntity<>(errMap, HttpStatus.BAD_REQUEST);
-    }
-
 }
