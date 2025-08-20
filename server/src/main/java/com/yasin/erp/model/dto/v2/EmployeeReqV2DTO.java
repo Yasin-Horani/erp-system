e64157b21cad77204e0b4c8ea5add33f1fa4be0a
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-08-20 16:28:58 +0200

    add validation for email and password

--- server/src/main/java/com/yasin/erp/model/dto/v2/EmployeeReqV2DTO.java
+++ server/src/main/java/com/yasin/erp/model/dto/v2/EmployeeReqV2DTO.java
@@ -1,7 +1,6 @@
 package com.yasin.erp.model.dto.v2;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,11 +11,24 @@ import lombok.NonNull;
 @AllArgsConstructor
 public class EmployeeReqV2DTO {
 
-    @NotNull
+    @NotNull(message = "You have to add employer name")
+    @Size(min = 3, max = 25, message = "Employee name must be between 3 and 25 characters")
     private String employeeName;
-    @NotNull
+
+    @NotNull(message = "You must provide a password")
+    @Size(min = 10, max = 30, message = "Password must be between 10 and 30 characters")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=(?:.*[a-z]){3,})(?=(?:.*\\d){3,})(?=.*[@!#$%^&*]).+$",
+            message = "Password must contain at least 1 uppercase letter, 3 lowercase letters, 3 digits, and 1 " +
+                    "special character (@!#$%^&*)"
+    )
     private String employeePassword;
-    @Email
-    @NotNull
+
+    @Email(message = "Invalid email format")
+    @NotNull(message = "Email is required gmail ro hotmail")
+    @Pattern(
+            regexp = "^[A-Za-z0-9._%+-]+@(gmail\\.com|hotmail\\.com)$",
+            message = "Only Gmail or Hotmail addresses are allowed"
+    )
     private String employeeEmail;
 }
