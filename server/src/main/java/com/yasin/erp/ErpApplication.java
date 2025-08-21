f17c2a6fcc2dee45b2f4f2560a454c393bbe00c9
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2025-08-21 21:51:42 +0200

    Set environment variables for Spring Boot to read

--- server/src/main/java/com/yasin/erp/ErpApplication.java
+++ server/src/main/java/com/yasin/erp/ErpApplication.java
@@ -1,21 +1,16 @@
 package com.yasin.erp;
 
-import io.github.cdimascio.dotenv.Dotenv;
+import com.yasin.erp.utilities.EnvLoaderUtility;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ErpApplication {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
 
 		// Set environment variables for Spring Boot to read
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
-		System.setProperty("CONTEXT_PATH", dotenv.get("CONTEXT_PATH"));
-
+		EnvLoaderUtility envLoaderUtility = new EnvLoaderUtility();
+		envLoaderUtility.loadIntoSystemProperties();
 		SpringApplication.run(ErpApplication.class, args);
 	}
 }
